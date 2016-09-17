9e5c3be527101ce2057c630f1d742a3b1bdc091a
Anastasiya Lobova <anastasiya.k91@gmail.com>
2016-09-17 13:29:03 +0300

    Перенесла переменную хранящую состояние лайка в данные

--- src/picture-data.js
+++ src/picture-data.js
@@ -6,43 +6,54 @@ var PictureData = function(data, index) {
   this.comments = this.data.comments;
   this.likes = this.data.likes;
   this.url = this.data.url;
+  this.liked = false;
 };
 
+/**Возвращает количество лайков*/
 PictureData.prototype.getLikes = function() {
   return this.likes;
 };
 
+/**Возвращает количество комментариев*/
 PictureData.prototype.getComments = function() {
   return this.comments;
 };
 
+/**При изменение лайков изменяет состояние и создает событие для оповещения*/
 PictureData.prototype.setLikesCount = function() {
-  var event = new CustomEvent('likes-count', {
-    detail: {
-      index: this.index
-    }
-  });
-
+  if (!this.liked) {
+    this.addLike();
+    this.liked = true;
+  } else {
+    this.removeLike();
+    this.liked = false;
+  }
+  var event = document.createEvent('Event');
+  event.initEvent('likes-count', true, true);
+  event.detail = {
+    index: this.index
+  };
   document.dispatchEvent(event);
 };
 
-PictureData.prototype.addLikes = function() {
+/**Прибавляет лайк*/
+PictureData.prototype.addLike = function() {
   this.likes++;
-  this.setLikesCount();
 };
 
-PictureData.prototype.subtractLikes = function() {
+/**Удаляет лайк*/
+PictureData.prototype.removeLike = function() {
   this.likes--;
-  this.setLikesCount();
 };
 
-PictureData.prototype.addComments = function() {
+/**Прибавляет комментарий*/
+PictureData.prototype.addComment = function() {
   this.comments++;
 };
 
-PictureData.prototype.subtractComments = function() {
+/**Прибавляет комментарий*/
+PictureData.prototype.removeComment = function() {
   this.comments--;
 };
-
 
 module.exports = PictureData;
