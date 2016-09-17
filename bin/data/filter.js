2f5ca5041646b5a4efb9bd9a9d344e729269b02c
Dmitrii Ermakov <dimurer@yandex-team.ru>
2016-09-17 16:07:40 +0300

    Перенес куски бекенда из другого проекта, тут их почему-то не было

--- bin/data/filter.js
+++ bin/data/filter.js
@@ -7,17 +7,18 @@ module.exports = function(list, filterID) {
       break;
     case 'filter-new':
       var newList = list.filter(function(item) {
-        return item.created < 3;
-      });
-
-      newList.sort(function (a, b) {
+        var TIME_AGO = Date.now() - (3 * 24 * 60 * 60 * 1000);
+        return item.created > TIME_AGO;
+      }).sort(function (a, b) {
         return b.created - a.created;
       });
+      return newList;
       break;
     case 'filter-discussed':
-      list.sort(function(a, b) {
+      var discussedList = list.sort(function(a, b) {
         return b.comments - a.comments;
       });
+        return discussedList;
       break;
     case 'default':
       return list;
